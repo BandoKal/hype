a53694dc1efc69ccaf5c113944544d7099fa6060
BandoKal <jason.bandy77@gmail.com>
2019-05-28 14:53:30 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "hype",
-            dependencies: []),
+            dependencies: [],
+            path: "hype/"),
         .testTarget(
             name: "hypeTests",
             dependencies: ["hype"]),
